f16b44ae2625c31f6889a4db610f7aa16c223a5e
shushirai <alber.eins92@gmail.com>
2025-10-08 08:19:05 -0700

    Add linux ver.

--- linux/build.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-docker build  -f Dockerfile_tf -t tf_bm .
-docker build -f Dockerfile_pytorch -t pytorch_bm .
--- linux/run_pytorch.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-docker run --gpus all -it --rm \
-        -v $(pwd):/workspace pytorch_bm \
-        bash
